76d2d48e9f8469d8fa5b7e79c9a580550a5e8d8c
Ertuğrul Demir <61366446+ertugrul-dmr@users.noreply.github.com>
2020-02-23 04:53:48 +0300

    Update collatz.py

--- collatz-sequence/collatz.py
+++ collatz-sequence/collatz.py
@@ -6,14 +6,6 @@ number is even, then collatz() should print number // 2 and return this value.
 If number is odd, then collatz() should print and return 3 * number + 1 .
 Then write a program that lets the user type in an integer and that keeps
 calling collatz() on that number until the function returns the value 1 .
-(Amazingly enough, this sequence actually works for any integer—sooner
-or later, using this sequence, you’ll arrive at 1! Even mathematicians aren’t
-sure why. Your program is exploring what’s called the Collatz sequence, some-
-times called “the simplest impossible math problem.”)
-Remember to convert the return value from input() to an integer with
-the int() function; otherwise, it will be a string value.
-Hint: An integer number is even if number % 2 == 0 , and it’s odd if
-number % 2 == 1 .
 """
 
 
