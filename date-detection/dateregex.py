7f0b2039cde3d1b9342806be0e80fd212f136cd2
Ertuğrul Demir <61366446+ertugrul-dmr@users.noreply.github.com>
2020-02-23 05:33:09 +0300

    Update dateregex.py

--- date-detection/dateregex.py
+++ date-detection/dateregex.py
@@ -2,33 +2,7 @@
 # Finds and prints date formats from the given string.
 import re
 
-dates = ''' 07.02.2020 20:00        BTC TURK YENİ MALATYASPOR   0 - 1   MKE ANKARAGÜCÜ  Detaylar Detaylar
-08.02.2020 14:00        İTTİFAK HOLDİNG KONYASPOR   0 - 0   YUKATEL DENİZLİSPOR     Detaylar Detaylar
-08.02.2020 17:00        GENÇLERBİRLİĞİ  0 - 2   TRABZONSPOR A.Ş.    Detaylar Detaylar
-08.02.2020 17:00        BEŞİKTAŞ A.Ş.   3 - 0   GAZİANTEP FUTBOL KULÜBÜ A.Ş.    Detaylar Detaylar
-08.02.2020 20:00        FENERBAHÇE A.Ş.     1 - 1   AYTEMİZ ALANYASPOR  Detaylar Detaylar
-09.02.2020 13:30        HES KABLO KAYSERİSPOR   2 - 2   FRAPORT-TAV ANTALYASPOR     Detaylar Detaylar
-09.02.2020 16:00        DEMİR GRUP SİVASSPOR    1 - 1   MEDİPOL BAŞAKŞEHİR FK   Detaylar Detaylar
-09.02.2020 19:00        KASIMPAŞA A.Ş.  0 - 3   GALATASARAY A.Ş.    Detaylar Detaylar
-10.02.2020 20:00        GÖZTEPE A.Ş.    -   ÇAYKUR RİZESPOR A.Ş.    Detaylar Detaylar
-14.02.2020 20:00       MEDİPOL BAŞAKŞEHİR FK   -   BEŞİKTAŞ A.Ş.   Detaylar Detaylar
-15.02.2020 14:00        YUKATEL DENİZLİSPOR     -   HES KABLO KAYSERİSPOR   Detaylar Detaylar
-15.02.2020 17:00        İTTİFAK HOLDİNG KONYASPOR   -   GÖZTEPE A.Ş.    Detaylar Detaylar
-15.02.2020 20:00        MKE ANKARAGÜCÜ  -   FENERBAHÇE A.Ş.     Detaylar Detaylar
-16.02.2020 13:30        AYTEMİZ ALANYASPOR  -   GENÇLERBİRLİĞİ  Detaylar Detaylar
-16.02.2020 16:00        TRABZONSPOR A.Ş.    -   DEMİR GRUP SİVASSPOR    Detaylar Detaylar
-16.02.2020 19:00        GALATASARAY A.Ş.    -   BTC TURK YENİ MALATYASPOR   Detaylar Detaylar
-17.02.2020 20:00        GAZİANTEP FUTBOL KULÜBÜ A.Ş.    -   ÇAYKUR RİZESPOR A.Ş.    Detaylar Detaylar
-17.02.2020 20:00        FRAPORT-TAV ANTALYASPOR     -   KASIMPAŞA A.Ş.  Detaylar Detaylar
-28.02.2020 20:00       AYTEMİZ ALANYASPOR  -   BEŞİKTAŞ A.Ş.   Detaylar Detaylar
-29.02.2020 14:00        İTTİFAK HOLDİNG KONYASPOR   -   KASIMPAŞA A.Ş.  Detaylar Detaylar
-29.02.2022 17:00        TRABZONSPOR A.Ş.    -   ÇAYKUR RİZESPOR A.Ş.    Detaylar Detaylar
-29.02.2021 20:00        FRAPORT-TAV ANTALYASPOR     -   FENERBAHÇE A.Ş.     Detaylar Detaylar
-01.03.2020 13:30        YUKATEL DENİZLİSPOR     -   BTC TURK YENİ MALATYASPOR   Detaylar Detaylar
-01.03.2020 16:00        MKE ANKARAGÜCÜ  -   DEMİR GRUP SİVASSPOR    Detaylar Detaylar
-01.03.2020 19:00        GALATASARAY A.Ş.    -   GENÇLERBİRLİĞİ  Detaylar Detaylar
-02.03.2020 20:00        MEDİPOL BAŞAKŞEHİR FK   -   GAZİANTEP FUTBOL KULÜBÜ A.Ş.    Detaylar Detaylar
-02.03.2020 20:00        HES KABLO KAYSERİSPOR   -   GÖZTEPE A.Ş.    Detaylar Detaylar'''
+dates = ''' your text goes here for search'''
 
 dateRegex = re.compile(r'''(
     (\d{2}) #DD
